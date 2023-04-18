9ce81be740dce8a5e544674502c02c16eb6b5a62
kplucien <kpluciennikk@gmail.com>
2023-04-18 09:58:30 +0200

    SpaceFinder UI update

--- MEPTools.tab/MEP.panel/toggles3.stack/SpaceFinder.pushbutton/spacefinder_script.py
+++ MEPTools.tab/MEP.panel/toggles3.stack/SpaceFinder.pushbutton/spacefinder_script.py
@@ -2,6 +2,8 @@
 import Autodesk.Revit.DB as DB
 from Autodesk.Revit.DB import *
 from System.Collections.Generic import List as cList
+from pyrevit import forms
+from Autodesk.Revit.UI import TaskDialog
 """from pyrevit import script
 output = script.get_output()"""
 
@@ -16,7 +18,9 @@ spaces = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_MEPSpaces)
 uidoc = __revit__.ActiveUIDocument
 doc = __revit__.ActiveUIDocument.Document
 
-space_number = raw_input("Type a space number:")
+#space_number = raw_input("Type a space number:")
+space_number= forms.ask_for_string(default="R.+xx.xxx", prompt="Space Number", title="SpaceFinder",)
+
 
 el_ids = []
 
@@ -28,7 +32,7 @@ for i in spaces:
 		ids = cList[ElementId](el_ids)
 		uidoc.Selection.SetElementIds(ids)
 if len(el_ids) == 0:
-    print "Space number not found."
+    TaskDialog.Show("Error", "Space Number {} not found".format(space_number))
 """output.close()"""
 
 
