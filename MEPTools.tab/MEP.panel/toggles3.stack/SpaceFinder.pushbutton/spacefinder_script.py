ca056c0808f6070dbf7ae71aa4157643049ecfbd
kplucien <kpluciennikk@gmail.com>
2023-04-27 11:44:49 +0200

    Space finder UI update

--- MEPTools.tab/MEP.panel/toggles3.stack/SpaceFinder.pushbutton/spacefinder_script.py
+++ MEPTools.tab/MEP.panel/toggles3.stack/SpaceFinder.pushbutton/spacefinder_script.py
@@ -1,45 +1,39 @@
-# coding: utf8
 import Autodesk.Revit.DB as DB
 from Autodesk.Revit.DB import *
 from System.Collections.Generic import List as cList
 from pyrevit import forms
 from Autodesk.Revit.UI import TaskDialog
-"""from pyrevit import script
-output = script.get_output()"""
-
-__doc__ = """Select a space by space number"""
-__title__ = "SpaceFinder"
 
 uidoc = __revit__.ActiveUIDocument
 doc = __revit__.ActiveUIDocument.Document
 
-
+####imports for UI
+import sys
+from pyrevit import forms
+from pyrevit import script
+#imports for UI
 spaces = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_MEPSpaces).WhereElementIsNotElementType().ToElements()
-uidoc = __revit__.ActiveUIDocument
-doc = __revit__.ActiveUIDocument.Document
 
-#space_number = raw_input("Type a space number:")
-space_number= forms.ask_for_string(default="R.+xx.xxx", prompt="Space Number", title="SpaceFinder",)
+class AboutWindow(forms.WPFWindow):
+
+    def __init__(self, xaml_file_name):
+        forms.WPFWindow.__init__(self, xaml_file_name)
+
+    def funkcja(self, sender, args):
+        space_number = self.space_nr.Text
+        el_ids = []
+
+        for i in spaces:
+            if i.Number == space_number:
+                #print i.Id
+
+                el_ids.append(i.Id)
+                ids = cList[ElementId](el_ids)
+                uidoc.Selection.SetElementIds(ids)
+        if len(el_ids) == 0:
+            TaskDialog.Show("Error", "Space Number {} not found".format(space_number))
+        self.Close()
+
+space_number = AboutWindow("ui.xaml").ShowDialog()
 
 
-el_ids = []
-
-for i in spaces:
-	if i.Number == space_number:
-		#print i.Id
-
-		el_ids.append(i.Id)
-		ids = cList[ElementId](el_ids)
-		uidoc.Selection.SetElementIds(ids)
-if len(el_ids) == 0:
-    TaskDialog.Show("Error", "Space Number {} not found".format(space_number))
-"""output.close()"""
-
-
-
-
-
-
-"""from rpw.ui.forms import TextInput
-value = TextInput('Title', default="3")
-print(value)"""
